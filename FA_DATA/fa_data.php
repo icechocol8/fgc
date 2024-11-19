59e5ccfde96739df92cf4353ed6df3d31ee73ed3
icechocol8 <123921862+icechocol8@users.noreply.github.com>
2024-11-19 14:57:24 +0800

    Add files via upload

---
+++ FA_DATA/fa_data.php
@@ -0,0 +1,19 @@
+
+<div id="content">
+    <table id="data-list">
+      <tr>
+        <th>ID</th>
+     
+        <th>Movement Date</th>
+        <th>Asset Tag</th>
+        <th>Asset Type</th>
+        <th>Workstation</th>
+        <th>Site</th>
+        <th>Assignee</th>
+        <th>Account/Department</th>
+        <th>Function</th>
+        <th>Manager</th>
+        <th>Designation</th>
+        <th>WS Designation</th>
+
+
