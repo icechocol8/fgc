cae0b67cbf85de6099331c6b6e3a131e89e3a141
icechocol8 <123921862+icechocol8@users.noreply.github.com>
2024-11-28 06:03:15 +0800

    Update index.php

--- FA_DATA/index.php
+++ FA_DATA/index.php
@@ -1,41 +1,37 @@
-<?php
-
-include_once 'fa_Data/fa_Data.php';
-
-include 'config/config.php';
-
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Asset Tracker</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Assistant&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <link rel="stylesheet" href="css/custom.css?<?php echo time();?>">
-
-    <body1>
-<div id="container">
-    <div id="header"></div>
-    <h2>ASSET TRACKER</h2>
-    </div>
-    <div id="wrapper">
-            <div id="menu">
-            <a href="main.php" class="home"><i class="fa-sharp fa-solid fa-house-chimney"></i> HOME</a> 
-                <a href="index.php?page=mc"><i class="mc"></i> MOVEMENT CHECKER</a>
-                <a href="index.php?page=wc" class="wc"><i class="fa-solid fa-calendar"></i> WORKSTATION CHECKER</a>
-                <a href="ASSET_CHECKER?page=ac" class="ac"><i class="fa-solid fa-wallet"></i> ASSET CHECKER</a>
-                <a href="FA_DATA/fdata.php" class="fdata"><i class="fa-solid fa-hand-holding-dollar"></i> FA DATA</a>
-                <a href="logout.php" class="move-right"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
-            </div>
-            
-    </div>
+<script>
+function showResults(str) {
+  if (str.length == 0) {
+    document.getElementById("search-result").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("search-result").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "fa-data/search.php?q=" + str, true);
+    xmlhttp.send();
+  }
+}
+</script>
+<div id="third-submenu">
+    <a href="index.php?page=employee&subpage=users">FA DATA LIST</a> |
+    <a href="index.php?page=employee&subpage=users&action=create">Add Data</a> |
+   
+    Search <input type="text" id="search" name="search" onkeyup="showResults(this.value)">
 </div>
-</body1>
-</html>
 
+<div id="subcontent">
+    <?php
+      switch($action){
+               
+               
+      
+             
+                default:
+                    require_once 'fa-data/fa_data.php';
+                break; 
+            }
+    ?>
+  </div>
